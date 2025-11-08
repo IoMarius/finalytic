6964e6998e9d7382543f33640ebc88b940c337a6
Marius Iordan <marius.iordan@smartgov.md>
2025-11-08 02:04:57 +0200

    Add startup logs in bot

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ def main():
     from core.commands import start_command, handle_image_command
     from data.database import init_db
     from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+    from core.logger import logger
     import os
     from dotenv import load_dotenv
 
@@ -10,11 +11,14 @@ def main():
 
     init_db()
 
+    logger.info("Configuring bot")
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(MessageHandler(filters.PHOTO, handle_image_command))
 
+    logger.info("Bot running.")
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
